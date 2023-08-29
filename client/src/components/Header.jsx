7c828c55f68aa37c5fc6eb0b0439154997d494ad
georgetheoracle <uche1694@student.babcock.edu.ng>
2023-08-29 03:51:34 +0100

    Nav-bar

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <div className='bg-slate-900 text-white'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+            <Link to='/'>
+                <h1 className='font-bold'>Auth app</h1>
+            </Link>
+            
+            <ul className='flex gap-4'>
+                <Link to='/'><li>Home</li></Link>
+                <Link to='/about'><li>About</li></Link>
+                <Link to='/sign-in'> <li>Sign In</li></Link>
+               
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Header
