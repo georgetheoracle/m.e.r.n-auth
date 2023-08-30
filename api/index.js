62a80aa9afa4fdeb10d330ad8b1fe48f34878fbb
georgetheoracle <uche1694@student.babcock.edu.ng>
2023-08-30 07:59:39 +0100

    middleware and err handling

--- api/index.js
+++ api/index.js
@@ -27,3 +27,14 @@ app.get('/', (req, res) => {
 });
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+// middleware and error handling
+app.use((err, req, res, next) =>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode
+    })
+})
