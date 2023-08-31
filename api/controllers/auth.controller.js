2c1bcb32ba62fb8e037e4b0ece535563425d70eb
georgetheoracle <uche1694@student.babcock.edu.ng>
2023-08-31 10:23:46 +0100

    signIn API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
-import bcryptjs from 'bcryptjs'
+import bcryptjs from 'bcryptjs';
 import { errorHandeler } from '../utils/error.js';
+import jwt from  'jsonwebtoken';
 
 export const signup = async(req, res, next) => {
     const { username, email, password} = req.body;
@@ -14,3 +15,19 @@ export const signup = async(req, res, next) => {
         next(error);
     }
 };
+
+export const signin =async (req, res, next) =>{
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({email: email});
+        if(!validUser) return next(errorHandeler(404, 'User not found'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandeler(401, 'wrong email or wrong password!'));
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        const { password: hashPassword, ...rest } = validUser._doc;
+        const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+        res.cookie('accesstoken', token, {httpOnly: true, expires: expiryDate}).status(200).json(rest);
+    } catch (error) {
+        next(error)
+    }
+}
